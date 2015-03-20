138c02e22b68013dbae083b1021dfabd1327d48b
Peterson, Justin <Justin_Peterson@intuit.com>
2015-03-19 21:36:28 -0700

    Add some initial content

--- player/src/js/components/Sidebar.react.js
+++ player/src/js/components/Sidebar.react.js
@@ -1,11 +1,16 @@
 'use strict';
 /* @jsx React.DOM */
 var React = require('react');
+var Strings = require('../strings')
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>Sidebar</div>
+      <div className="sidebar-container">
+        <img src="./public/images/profile.jpg" className="sidebar-profile"/>
+        <h3>About</h3>
+        {Strings.about}
+      </div>
     );
   },
 });
---
+++ player/src/js/strings.js
@@ -0,0 +1,7 @@
+var about = "I am a software engineer entering my final year at the Rochester Institute of Tech. Working in JS, Java and Clojure to create all things web.";
+var aboutFooter = "Current gig: Intuit [TurboTax Online]";
+
+module.exports = {
+  about: about,
+  aboutFooter: aboutFooter
+}
